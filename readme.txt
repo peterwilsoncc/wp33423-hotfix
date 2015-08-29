69f1f3e13e9fe30f755c72f71bad07fc64fa9833
Peter Wilson <git-commit-by@peterwilson.cc>
2015-08-29 11:13:07 +1000

    Initial version 1.0

---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== wp33423 hotfix ===
+Contributors: peterwilsoncc
+Tags: hotfix, terms, taxonomy
+Requires at least: 4.3
+Tested up to: 4.3
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This is a hotfix for the ticket #33423.
+
+== Description ==
+
+Hotfix for [ticket 33423](https://core.trac.wordpress.org/ticket/33423).
+
+Reversed arguments for the scheduled task to split terms can cause the database to balloon in some edge cases. This fixes the problem.
+
+A fix will be included in WP 4.3.1 and this plugin will automatically disable itself.
+
+== Installation ==
+
+Install this from the WordPress dashboard.
+
+== Changelog ==
+
+1.0 Initial version.
---
+++ wp33423-hotfix.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: wp33423 hotfix
+Description: This is a hotfix for the ticket #33423.
+Author: peterwilsoncc
+Author URI: http://peterwilson.cc/
+License: GPLv2 or later
+*/
+
+
+function wp33423_hotfix(){
+  global $wp_version;
+  /**
+   * Disable this plugin from 4.3.1
+   */
+  if ( ( 1 !== version_compare( "4.3.1", $wp_version ) ) && current_user_can( 'activate_plugins' ) ) {
+    deactivate_plugins( plugin_basename( __FILE__ ) );
+  }
+  
+  /**
+   * Prevent 4.3 from messing up the cron array and options table
+   */
+  remove_action( 'admin_init', '_wp_check_for_scheduled_split_terms' );
+
+  /**
+   * Clean the cron array after 4.3 messed it up
+   */
+  $cron_array = _get_cron_array();
+  if ( isset( $cron_array['wp_batch_split_terms'] ) ) {
+    unset( $cron_array['wp_batch_split_terms'] );
+    _set_cron_array( $cron_array );
+  }
+  
+  /**
+   * In order to avoid the wp_batch_split_terms() job being accidentally removed,
+   * check that it's still scheduled while we haven't finished splitting terms.
+   */
+  if ( ! get_option( 'finished_splitting_shared_terms' ) && ! wp_next_scheduled( 'wp_split_shared_term_batch' ) ) {
+    wp_schedule_single_event( time() + MINUTE_IN_SECONDS, 'wp_split_shared_term_batch' );
+  }
+}
+
+add_action( 'admin_init', 'wp33423_hotfix', 5 );
